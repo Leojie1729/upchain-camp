9be3c219e2f926a45826af4ea5980dda911414c9
Leojie1729 <leojie1729@gmail.com>
2023-03-12 22:00:11 +0800

    add

---
+++ w1_1/Counter.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint public count;
+
+    constructor() {
+        count = 0;
+    }
+
+    function add(uint amount) public {
+        count = count + amount;
+    }
+}
